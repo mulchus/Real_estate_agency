78117df23e3254129691826025e6536573b136a3
Юрий <mulch@list.ru>
2023-03-24 12:38:28 +0300

    Made data-migrate set_normalize_phonenumber

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Claim
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
     list_editable = ['new_building']
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)

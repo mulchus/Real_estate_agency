5662f89b51bf74eca6ba0fdda706e55a2e24bfed
mulchus <mulch@list.ru>
2023-03-26 15:32:17 +0300

    Set to display flats in owner table

--- property/admin.py
+++ property/admin.py
@@ -8,20 +8,24 @@ from .models import Owner
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner', 'owners_phonenumber')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber',
+                    'owner_pure_phone')
     list_editable = ['new_building']
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)
 
 
 class OwnerAdmin(admin.ModelAdmin):
-    search_fields = ('owner', 'owner_pure_phone', 'flat')
+    search_fields = ['owner', 'flat__address', 'owner_pure_phone']
     raw_id_fields = ('flat',)
-    list_display = ('owner', 'owner_pure_phone')
+    list_display = ('owner', 'owner_pure_phone', 'get_flats')
+
+    def get_flats(self, obj):
+        return ", ".join([flat.__str__() for flat in obj.flat.all()])
 
 
 class ClaimAdmin(admin.ModelAdmin):
-    search_fields = ('username', 'flat')
+    search_fields = ['username__username', 'flat__address']
     raw_id_fields = ('flat',)
     list_display = ('username', 'flat', 'claim_text')
 
